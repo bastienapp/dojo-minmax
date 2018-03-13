c37b66c9ff983a1cc8821f06c8fb06218f9abb09
bastienwcs <bastien@wildcodeschool.fr>
2018-03-13 09:55:17 +0100

    fix

--- MinmaxTest.java
+++ MinmaxTest.java
@@ -1,9 +1,37 @@
 import junit.framework.*;
 import org.junit.Test;
+import org.junit.Assert;
 
 public class MinmaxTest extends TestCase {
 	@Test
 	public void testTrue() throws Exception {
 		assertEquals(true, true);
+	}
+
+	@Test
+	public void test1() throws Exception {
+		int[] arrayWithValues = new int[]{0, 1};
+		Assert.assertArrayEquals(arrayWithValues, Minmax.foundMinMax(arrayWithValues));
+	}
+
+	@Test
+	public void test2() throws Exception {
+		int[] arrayWithValues = new int[]{0, 1, 2, 3};
+  		int[] tableauMinmax = new int[]{0, 3};
+		Assert.assertArrayEquals(tableauMinmax, Minmax.foundMinMax(arrayWithValues));
+	}
+
+	@Test
+	public void test3() throws Exception {
+		int[] arrayWithValues = new int[]{-6};
+  		int[] tableauMinmax = new int[]{-6, -6};
+		Assert.assertArrayEquals(tableauMinmax, Minmax.foundMinMax(arrayWithValues));
+	}
+
+	@Test
+	public void test4() throws Exception {
+		int[] arrayWithValues = new int[]{3, 1, -4, 23, 12, 5};
+  		int[] tableauMinmax = new int[]{-4, 23};
+		Assert.assertArrayEquals(tableauMinmax, Minmax.foundMinMax(arrayWithValues));
 	}
 }
--- solution/MinmaxTest.java
+++ solution/MinmaxTest.java
@@ -1,6 +1,6 @@
 import junit.framework.*;
 import org.junit.Test;
-import java.util.Arrays;
+import org.junit.Assert;
 
 public class MinmaxTest extends TestCase {
 	@Test
@@ -10,28 +10,28 @@ public class MinmaxTest extends TestCase {
 
 	@Test
 	public void test1() throws Exception {
-  		int[] tableauEntier = new int[]{0,1};
-		assertEquals(Arrays.toString(tableauEntier), Arrays.toString(Minmax.foundMinMax(tableauEntier)));
+		int[] arrayWithValues = new int[]{0, 1};
+		Assert.assertArrayEquals(arrayWithValues, Minmax.foundMinMax(arrayWithValues));
 	}
 
 	@Test
 	public void test2() throws Exception {
-  		int[] tableauEntier = new int[]{0,1,2,3};
-  		int[] tableauMinmax = new int[]{0,3};
-		assertEquals(Arrays.toString(tableauMinmax), Arrays.toString(Minmax.foundMinMax(tableauEntier)));
+		int[] arrayWithValues = new int[]{0, 1, 2, 3};
+  		int[] tableauMinmax = new int[]{0, 3};
+		Assert.assertArrayEquals(tableauMinmax, Minmax.foundMinMax(arrayWithValues));
 	}
 
 	@Test
 	public void test3() throws Exception {
-  		int[] tableauEntier = new int[]{-6};
-  		int[] tableauMinmax = new int[]{-6,-6};
-		assertEquals(Arrays.toString(tableauMinmax), Arrays.toString(Minmax.foundMinMax(tableauEntier)));
+		int[] arrayWithValues = new int[]{-6};
+  		int[] tableauMinmax = new int[]{-6, -6};
+		Assert.assertArrayEquals(tableauMinmax, Minmax.foundMinMax(arrayWithValues));
 	}
 
 	@Test
 	public void test4() throws Exception {
-  		int[] tableauEntier = new int[]{3,1,-4,23,12,5};
-  		int[] tableauMinmax = new int[]{-4,23};
-		assertEquals(Arrays.toString(tableauMinmax), Arrays.toString(Minmax.foundMinMax(tableauEntier)));
+		int[] arrayWithValues = new int[]{3, 1, -4, 23, 12, 5};
+  		int[] tableauMinmax = new int[]{-4, 23};
+		Assert.assertArrayEquals(tableauMinmax, Minmax.foundMinMax(arrayWithValues));
 	}
 }
