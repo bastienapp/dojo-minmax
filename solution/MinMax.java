951c3bb6033c747945dd5c22dd4a20936a3f3604
Bastien Krettly <bastien@wildcodeschool.fr>
2018-03-15 10:37:32 +0100

    Update MinMax.java

--- solution/MinMax.java
+++ solution/MinMax.java
@@ -1,16 +1,29 @@
 class MinMax {
 
-	public static int[] foundMinMax(int[] tableauEntier) {
-		int min = tableauEntier[0];
-		int max = tableauEntier[0];
-		for (int i = 0; i < tableauEntier.length; i++) {
-			if (tableauEntier[i] < min) {
-				min = tableauEntier[i];
-			}
-			if (tableauEntier[i] > max) {
-				max = tableauEntier[i];
-			}
-		}
-		return new int[]{min, max};
-	}
+	public static int[] foundMinMax(int[] tableau) {
+
+        // initialiser min avec la premiere valeur du tableau 
+        int min = tableau[0];
+        int max = tableau[0];
+
+        // parcourir les valeurs du tableau 
+
+        // i  0  1   2  3  4   5   6
+        // [ 12, 4, 54, 2, 8, 23 ]     tableau.length: 6        
+        for (int i = 0; i < tableau.length; i = i + 1){
+            // si la valeur de la case est inférieure à min
+            if (tableau [ i ] < min ){
+                // min prend la valeur de la case
+                min = tableau[i];
+            }
+           
+            if (tableau [i] > max ) {
+                max = tableau [i];
+            }
+            
+        }
+        // retourner la valeur la plus petite du tableau : min
+        return new int[] {min,max};
+    }
+
 }
